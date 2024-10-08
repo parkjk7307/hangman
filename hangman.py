040896f59ef55b719ad53788df60c8a7520b7444
박진경 <76420796+parkjk7307@users.noreply.github.com>
2024-10-08 11:40:38 +0900

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -123,7 +123,7 @@ def checkWrongAnswer(missedLetters, secretWord):
             
 def main():
     """Main application entry point."""
-    print('H A N G M A N by JK')
+    print('H A N G M A N by JK on 2024/10/08')
     missedLetters = ''
     correctLetters = ''
     gameSucceeded = False
